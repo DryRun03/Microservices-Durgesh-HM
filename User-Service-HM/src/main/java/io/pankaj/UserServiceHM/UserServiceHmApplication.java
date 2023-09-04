75b22083428fccc67497f703cdfa5087ec714bd1
Pankaj Garg <pankaj.garg@oodles.io>
2023-09-04 16:48:04 +0530

    UserService-HM

---
+++ User-Service-HM/src/main/java/io/pankaj/UserServiceHM/UserServiceHmApplication.java
@@ -0,0 +1,13 @@
+package io.pankaj.UserServiceHM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceHmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceHmApplication.class, args);
+	}
+
+}
---
+++ User-Service-HM/src/test/java/io/pankaj/UserServiceHM/UserServiceHmApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pankaj.UserServiceHM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceHmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
